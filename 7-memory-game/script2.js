2cdfd55a98f07192a9f5f979e0ec540f65dd1f61
SugamChaudharry <nextgenachieverss@gmail.com>
2023-10-15 11:38:23 +0530

    .

--- 7-memory-game/script2.js
+++ 7-memory-game/script2.js
@@ -1,4 +1,21 @@
-const mainEl = document.getElementsByClassName("main");
-const box = document.querySelectorAll(".box");
+const mainEl = document.querySelector(".main");
+const boxEl = document.getElementsByClassName("box");
 
-
+arr = [];
+for (let i = 0, a = 1 ,b =''; i < 6 * 6; i++, a === 18 ? (a = 1) : a++, arr.push(b)) {
+    b = `<div class="box"><p>${a}</></div>`;
+}
+function toHtml(array) {
+    arr.sort(function () {
+        return 0.5 - Math.random();
+    });
+    arr.forEach(element => {
+        mainEl.innerHTML += element;
+    });
+}
+toHtml(arr);
+for (const start of boxEl) {
+    start.addEventListener("click", function onClick() {
+        start.firstElementChild.style.display = (start.firstElementChild.style.display === '')?'block': '';
+    });
+}
