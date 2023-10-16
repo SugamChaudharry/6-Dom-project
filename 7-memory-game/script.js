053c43a2d6017b40d9de7137e31324025d1ee28d
SugamChaudharry <nextgenachieverss@gmail.com>
2023-10-16 07:39:13 +0530

    compelted

--- 7-memory-game/script.js
+++ 7-memory-game/script.js
@@ -3,12 +3,15 @@ const startEl = document.getElementsByClassName("start");
 for (const start of startEl) {
     start.addEventListener("click", function onClick() {
         start.innerHTML = (start.innerHTML === '')?`<p>START</p>`:'';
-        mached()
+        match()
     });
 }
 
-function mached() {
+function match() {
     if (startEl[0].innerHTML === startEl[1].innerHTML && startEl[0].innerHTML !== '' && startEl[1].innerHTML !== '') {
-        window.location.pathname = "7-memory-game/game.html";
+        setTimeout(() => {
+            window.location.pathname = "7-memory-game/game.html";
+        }, 1000);
+
     }
 }
--- 7-memory-game/script2.js
+++ 7-memory-game/script2.js
@@ -1,11 +1,15 @@
 const mainEl = document.querySelector(".main");
 const boxEl = document.getElementsByClassName("box");
+const numberEl = document.getElementsByClassName("number")
+
 
 arr = [];
+
 for (let i = 0, a = 1 ,b =''; i < 6 * 6; i++, a === 18 ? (a = 1) : a++, arr.push(b)) {
-    b = `<div class="box"><p>${a}</></div>`;
+    b = `<div class="box"><p>${a}</p></div>`;
 }
-function toHtml(array) {
+
+function toHtml(array) {    
     arr.sort(function () {
         return 0.5 - Math.random();
     });
@@ -14,8 +18,33 @@ function toHtml(array) {
     });
 }
 toHtml(arr);
-for (const start of boxEl) {
-    start.addEventListener("click", function onClick() {
-        start.firstElementChild.style.display = (start.firstElementChild.style.display === '')?'block': '';
-    });
+
+console.log(boxEl);
+
+for (const iterator of boxEl) {
+    iterator.addEventListener('click',()=>{
+        iterator.classList[1] === undefined
+            ? iterator.classList.add("ShowFont")
+            : iterator.classList.remove("ShowFont")
+        console.log(iterator.classList);
+        setTimeout(() => {
+            const ShowFontEl = document.querySelectorAll(".ShowFont")
+            console.log(ShowFontEl.length);
+            if (ShowFontEl.length > 1) {
+                if (ShowFontEl[0].innerHTML === ShowFontEl[1].innerHTML) {
+                    ShowFontEl[0].classList.add('boxMatch')
+                    ShowFontEl[1].classList.add('boxMatch')
+
+                    ShowFontEl[0].classList.remove('ShowFont')
+                    ShowFontEl[1].classList.remove('ShowFont')
+                    if (document.querySelectorAll(".boxMatch").length == arr.length) {
+                        alert('win')
+                    }
+                }else{
+                    ShowFontEl[0].classList.remove("ShowFont");
+                    ShowFontEl[1].classList.remove("ShowFont");
+                }
+            }
+        }, 2000);
+    })
 }
