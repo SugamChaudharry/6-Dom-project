918a3ee56068d578b00da5631f61e3ced6593cdb
SugamChaudharry <nextgenachieverss@gmail.com>
2023-12-16 12:46:50 +0530

    Revert "."
    This reverts commit a33a9cf220119208fb30259923f095d145e5c106.

---
+++ 7-memory-game/script.js
@@ -0,0 +1,20 @@
+const startEl = document.getElementsByClassName("start");
+
+for (const start of startEl) {
+  start.addEventListener("click", function onClick() {
+    start.innerHTML = start.innerHTML === "" ? `<p>START</p>` : "";
+    match();
+  });
+}
+
+function match() {
+  if (
+    startEl[0].innerHTML === startEl[1].innerHTML &&
+    startEl[0].innerHTML !== "" &&
+    startEl[1].innerHTML !== ""
+  ) {
+    setTimeout(() => {
+      window.location.pathname = "7-memory-game/game.html";
+    }, 1000);
+  }
+}
---
+++ 7-memory-game/script2.js
@@ -0,0 +1,49 @@
+const mainEl = document.querySelector(".main");
+const boxEl = document.getElementsByClassName("box");
+const numberEl = document.getElementsByClassName("number");
+
+arr = [];
+
+for (let i = 0, a = 1, b = ""; i < 6 * 6; i++, a === 18 ? (a = 1) : a++, arr.push(b)) {
+  b = `<div class="box"><p>${a}</p></div>`;
+}
+
+function toHtml(array) {
+  arr.sort(function () {
+    return 0.5 - Math.random();
+  });
+  arr.forEach((element) => {
+    mainEl.innerHTML += element;
+  });
+}
+toHtml(arr);
+
+console.log(boxEl);
+
+for (const iterator of boxEl) {
+  iterator.addEventListener("click", () => {
+    iterator.classList[1] === undefined
+      ? iterator.classList.add("ShowFont")
+      : iterator.classList.remove("ShowFont");
+    console.log(iterator.classList);
+    setTimeout(() => {
+      const ShowFontEl = document.querySelectorAll(".ShowFont");
+      console.log(ShowFontEl.length);
+      if (ShowFontEl.length > 1) {
+        if (ShowFontEl[0].innerHTML === ShowFontEl[1].innerHTML) {
+          ShowFontEl[0].classList.add("boxMatch");
+          ShowFontEl[1].classList.add("boxMatch");
+
+          ShowFontEl[0].classList.remove("ShowFont");
+          ShowFontEl[1].classList.remove("ShowFont");
+          if (document.querySelectorAll(".boxMatch").length == arr.length) {
+            alert("win");
+          }
+        } else {
+          ShowFontEl[0].classList.remove("ShowFont");
+          ShowFontEl[1].classList.remove("ShowFont");
+        }
+      }
+    }, 2000);
+  });
+}
